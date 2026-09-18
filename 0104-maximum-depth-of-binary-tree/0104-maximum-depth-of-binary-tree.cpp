1ee6fca3beea7bda26b2f2cb64784fa5d3bd9e70
Dev kumar <120113508+Devkumar-web@users.noreply.github.com>
2026-09-18 14:11:52 +0530

    Time: 0 ms (100%), Space: 22.2 MB (27.15%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -24,20 +24,37 @@ public:
     int maxDepth(TreeNode* root) {
         
         //lets do bfs
-        queue<TreeNode*> q;
-        q.push(root);
-        int count=0;
-        while(!q.empty() && root ){
-            int loop=q.size();
-            for(int i=0;i<loop;i++){
-                TreeNode* node=q.front();
-                q.pop();
-                if(node->left)q.push(node->left);
-                if(node->right)q.push(node->right);
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // int count=0;
+        // while(!q.empty() && root ){
+        //     int loop=q.size();
+        //     for(int i=0;i<loop;i++){
+        //         TreeNode* node=q.front();
+        //         q.pop();
+        //         if(node->left)q.push(node->left);
+        //         if(node->right)q.push(node->right);
 
-            }
-            count++;
+        //     }
+        //     count++;
+        // }
+        // return count;
+
+        stack<pair<TreeNode*,int>> st;
+        int ans=0;
+        if(root==nullptr){
+            return ans;
         }
-        return count;
+        //lets see what we can do
+        st.push({root,1});
+        while(!st.empty()){
+            pair<TreeNode*,int> p=st.top();
+            ans=max(ans,p.second);
+            st.pop();
+            if(p.first->left) st.push({p.first->left,p.second+1});
+            if(p.first->right) st.push({p.first->right,p.second+1});
+        }
+
+        return ans;
     }
 };
